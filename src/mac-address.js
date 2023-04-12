abb0c68cf4a404c825c7705c335af9c4508b2601
Nickoff <v3136820@gmail.com>
2023-04-12 11:44:08 +0300

    feat: comleted: mac-address, get-email-domain (Wed, Apr 12, 2023 11:44:07 AM)

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  return n.split('-').map(item => '0x' + item).map(element => {if (Number(element) <= 255) {} else {return false};}).indexOf(false) < 0 ? true : false;
 }
 module.exports = {
   isMAC48Address
