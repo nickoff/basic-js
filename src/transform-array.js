e5f8e1d3e7c64ba683bdf63e13ced03c7fa04c9e
Nickoff <v3136820@gmail.com>
2023-04-17 21:49:10 +0300

    feat: comleted: transform-array  (Mon, Apr 17, 2023 09:49:09 PM)

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(Array.isArray(arr) && !arr.length) {return []};
+  if(!Array.isArray(arr)) {throw new Error('\'arr\' parameter must be an instance of the Array!')};
+  if(!arr.includes('--double-next') && !arr.includes('--double-prev') && !arr.includes('--discard-prev') && !arr.includes('--discard-next')) {return arr};
+  
+  console.debug(arr)
+  let arrResult = [];
+  arrResult = arrResult.concat(arr)
+  
+  if(arr[0] === '--double-prev' || arr[0] === '--discard-prev') {arrResult = arr.slice(1, arr.length)}
+  if(arr[arr.length - 1] === '--double-next' || arr[arr.length - 1] === '--discard-next') {arrResult = arrResult.slice(0, arrResult.length - 1)}
+
+  if(arrResult.includes('--double-prev')) {
+      let indexDoublePrev = arrResult.indexOf('--double-prev')
+      if(indexDoublePrev == 1 || (arrResult[indexDoublePrev - 2] != '--discard-next')){arrResult[indexDoublePrev] = arrResult[indexDoublePrev - 1]}
+      else {delete arrResult[indexDoublePrev]; arrResult = arrResult.filter(element => element != undefined)}
+  }
+
+  if(arrResult.includes('--double-next')) {
+      let indexDoubleNext = arrResult.indexOf('--double-next')
+      arrResult[indexDoubleNext] = arrResult[indexDoubleNext + 1]
+  }
+
+  if(arrResult.includes('--discard-prev')) {
+      let indexDiscardPrev = arrResult.indexOf('--discard-prev')
+      if(arrResult[indexDiscardPrev - 2] != '--discard-next'){delete arrResult[indexDiscardPrev]; delete arrResult[indexDiscardPrev - 1]; arrResult = arrResult.filter(element => element != undefined)}
+      else{delete arrResult[indexDiscardPrev]; arrResult = arrResult.filter(element => element != undefined)}
+  }
+
+  if(arrResult.includes('--discard-next')) {
+      let indexDiscardNext = arrResult.indexOf('--discard-next')
+      delete arrResult[indexDiscardNext]; delete arrResult[indexDiscardNext + 1]; arrResult = arrResult.filter(element => element != undefined)
+
+  }
+
+
+
+
+  return arrResult
+
+
 }
 
 module.exports = {
